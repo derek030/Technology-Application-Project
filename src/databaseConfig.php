ee19e5f6f5421168618c897a8c0e1d1632164e0c
DEREK <hitmanreborn1211@gmail.com>
2023-10-12 16:59:15 +1100

    update database config

--- src/databaseConfig.php
+++ src/databaseConfig.php
@@ -3,7 +3,7 @@
     $servername = "localhost";
     $username = "root";
     $password = "";
-    $dbname = "houseofberks";
+    $dbname = "houseofbarks";
 
     $conn = new mysqli($servername, $username, $password, $dbname);
 
